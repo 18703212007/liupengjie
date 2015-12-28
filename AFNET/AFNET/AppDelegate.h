8ec3b7b3c1aaa3ad349f8b3456d4a7c8b577a01b
lpj <76930832@qq.com>
2015-12-28 14:27:37 +0800

    初始化

---
+++ AFNET/AFNET/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNET
+//
+//  Created by lanou3g on 15/12/28.
+//  Copyright © 2015年 com.lanou3g. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
